096c9be22c2671ec053c455bdb689e550e017822
GeorgeHolas <georgeholas404@gmail.com>
2024-05-15 09:37:42 -0600

    Initinal commit

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -7,18 +7,27 @@ const Inputs = () => {
         <input 
         type="text" 
         placeholder="Search by city..." 
-        className="text-xl text-black font-light p-2 w-full shadow-xl focus:outline-none 
+        className="text-xl text-gray-500 font-light p-2 w-full shadow-xl focus:outline-none 
         capitalize placeholder:lowercase" 
         />
 
         <BiSearch 
         size={30} 
-        className="text-white mx-2 cursor-pointer transition ease-out hover:scale-125" 
+        className="cursor-pointer transition ease-out hover:scale-125" 
         />
         <BiCurrentLocation 
         size={30} 
-        className="text-white mx-2 cursor-pointer transition ease-out hover:scale-125" 
+        className= "cursor-pointer transition ease-out hover:scale-125" 
         />
+      </div>
+      <div className="flex flex-row w-1/4 items-center justify-center">
+        <button className="text-2xl transition ease-out hover:scale-125">
+          °C
+        </button>
+        <p className="text-2xl font-medium mx-1">|</p>
+        <button className="text-2xl transition ease-out hover:scale-125">
+          °F
+        </button>
       </div>
     </div>
   );
