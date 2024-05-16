faed62ec6f974c60ff44a1f2dad1da53aa1c8392
GeorgeHolas <georgeholas404@gmail.com>
2024-05-16 10:53:51 -0600

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
-import { FaReact } from "react-icons/fa";
+import { useEffect, useState } from 'react';
 import TopButtons from "./components/TopButtons"; 
 import Inputs from "./components/Inputs";
+import TimeLocation from "./components/TimeLocation";
+import TempetatureDetails from "./components/TempetatureDetails";
+import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/WeatherService";
 
 const App = () => {
+  const [query, setQuery] = useState({ q: "Dublin" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  const getWeather = async () => {
+    await getFormattedWeatherData({ ...query, units }).then((data) => {
+      setWeather(data);
+      console.log(data);
+    });
+  };
+
+  useEffect(() => { getWeather(); }, [query, units]);
+
   return (
     <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
-      <TopButtons />
+      <TopButtons setQuery={setQuery} />
       <Inputs />
-    </div>
-  )
-}
+       
+      {weather && (
+       <>
+        <TimeLocation weather={weather} />
+        <TempetatureDetails weather={weather} />
+        <Forecast title="3 hour forecast" data={weather.hourly} />
+        <Forecast title="Daily forecast" data={weather.daily} />
+      </>
+     )}
+   </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,44 +1,44 @@
 import React from 'react'
 
-const TopButtons = () => {
-
-    const cities = [
-        {
-            id:1,
-            title: "London"
-        },
-        {
-            id:2,
-            title: "Monterrey"
-        },
-        {
-            id:3,
-            title: "Prague"
-        },
-        {
-            id:4,
-            title: "Anchorage"
-        },
-        {
-            id:5,
-            title: "Sydney"
-        }
+const TopButtons = ({setQuery}) => {
+  const cities = [
+      {
+        id: 1,
+        title: "London"
+      },
+      {
+        id: 2,
+        title: "Monterrey"
+      },
+      {
+        id: 3,
+        title: "Prague"
+      },
+      {
+        id: 4,
+        title: "Anchorage"
+      },
+      {
+        id: 5,
+        title: "Sydney"
+      },
     ]
 
   return (
     <div className='flex items-center justify-around my-6'>
-        {
-            cities.map((city) => (
-              <button
-                key={city.id}
-                className="text-lg font-medium hover:text-black px-3 py-2 rounded-nd transition ease-in"
-              >
-                {city.title}
-              </button>
-            ))
+      {
+        cities.map((city) => (
+          <button
+            key={city.id}
+            className="text-lg font-medium hover:text-black px-3 py-2 rounded-nd transition ease-in"
+            onClick={() => setQuery({q: city.title})}
+          >
+            {city.title}
+            </button>
+          ))
         }
     </div>
-  )
-}
+  );
+};
 
 export default TopButtons
