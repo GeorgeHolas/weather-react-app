3305dacb925627baa00df457544cef6cc6ecc468
GeorgeHolas <georgeholas404@gmail.com>
2024-05-17 09:45:30 -0600

    Inputs.jsx - done

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,13 +1,17 @@
+// Inputs.jsx
 import { useState } from "react";
 import { BiSearch, BiCurrentLocation } from "react-icons/bi";
 
+// Inputs component
 const Inputs = ({ setQuery, setUnits }) => {
   const [city, setCity] = useState("");
-
+  
+  // handle on click when user clicks search icon button
   const handleSearchClick = () => {
     if (city !== "") setQuery({ q: city });
   };
-
+  
+  // handle on click when user clicks on the current location button
   const handleLocationClick = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -19,6 +23,8 @@ const Inputs = ({ setQuery, setUnits }) => {
 
   return (
     <div className="flex flex-row justify-center my-6">
+
+      {/* Input and buttons container */}
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
         <input 
         value = {city}
@@ -29,11 +35,14 @@ const Inputs = ({ setQuery, setUnits }) => {
         capitalize placeholder:lowercase" 
         />
 
+        {/* Search icon button */}
         <BiSearch 
         size={30} 
         className="cursor-pointer transition ease-out hover:scale-125" 
         onClick={handleSearchClick}
         />
+
+        {/* Current location icon button */}
         <BiCurrentLocation 
         size={30} 
         className= "cursor-pointer transition ease-out hover:scale-125" 
