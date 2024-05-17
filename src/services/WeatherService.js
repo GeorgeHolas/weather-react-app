b960d6f82cf98a28b7fd4fec080389da9bf4ee2b
GeorgeHolas <georgeholas404@gmail.com>
2024-05-17 09:55:54 -0600

    WeatherService.js - done

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,8 +1,12 @@
+// WeatherService.js
 import { DateTime } from "luxon";
 
+// API key for OpenWeatherMap
 const API_KEY = "3e332d325f3ba7128dddb77469c5f63d"
+// Base URL for OpenWeatherMap API
 const BASE_URL = "https://api.openweathermap.org/data/2.5"
 
+// Function to get weather data from the API
 const getWeatherData = (infoType, searchParams) => {
     const url = new URL(BASE_URL + "/" + infoType);
     url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
@@ -10,9 +14,11 @@ const getWeatherData = (infoType, searchParams) => {
     return fetch(url).then((res) => res.json())    
 };
 
+// Function to get the icon URL from the icon code
 const iconUrlFromCode = (icon) =>
   `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
+// Function to format time to local time
 const formatToLocalTime = (
   secs, 
   offset, 
@@ -23,6 +29,7 @@ const formatToLocalTime = (
     .toFormat(format);
 };
 
+// Function to format current weather data
 const formatCurrent = (data) => {
     const {
       coord: {lat, lon}, 
@@ -60,17 +67,18 @@ const { main: details, icon } =  weather[0];
   };
 };
 
+// Function to format forecast weather data
 const formatForecastWeather = (secs, offset, data) => {
-    //hourly
+  // Format hourly forecast data
   const hourly = data.filter((f) => f.dt > secs).map((f) => ({
        temp: f.main.temp,
        title: formatToLocalTime(f.dt, offset, "hh:mm a"),
        icon: iconUrlFromCode(f.weather[0].icon),
        date: f.dt_txt,
     }))
-    .slice(0, 5)
+    .slice(0, 5) // Get the first 5 hourly forecasts
 
-    //daily
+  //Format daily forecast data
   const daily = data.filter((f) => f.dt_txt.slice(-8) === "00:00:00").map((f) => ({
         temp: f.main.temp,
         title: formatToLocalTime(f.dt, offset, "ccc"),
@@ -80,6 +88,7 @@ const formatForecastWeather = (secs, offset, data) => {
     return { hourly, daily };
 };
 
+// Function to get and format weather data
 const getFormattedWeatherData = async (searchParams) => {
   const formattedCurrentWeather = await getWeatherData(
     "weather",
@@ -88,12 +97,14 @@ const getFormattedWeatherData = async (searchParams) => {
 
   const { dt, lat, lon , timezone }  = formattedCurrentWeather  
   
+  // Get forecast weather data and format it
   const formattedForecastWeather = await getWeatherData("forecast", { 
     lat, 
     lon, 
     units: searchParams.units,
   }).then((d) => formatForecastWeather(dt, timezone, d.list));
   
+  // Return combined current and forecast weather data
   return { ...formattedCurrentWeather, ...formattedForecastWeather };
 };
 
